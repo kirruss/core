7413b5ee769b6507d57d6e2cab8498874f392339
BlueGhostGH <engisoftleaderoff@gmail.com>
2020-09-14 16:08:02 +0300

    docs: added jsdoc annotations to exported types

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,12 @@
 export type Fn<A, B> = (v: A) => B
+
+/**
+ * A generic asynchronous function that has an input of type
+ * T and an optional output of type U
+ */
 export type Task<T, U> = Fn<T, Promise<U | null>>
+
+/**
+ * A Task whose both input and output are of type T
+ */
 export type EndoTask<T> = Task<T, T>
