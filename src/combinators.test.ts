28a12487b959d58eaa0f7d669d19d61532a6bc42
BlueGhostGH <engisoftleaderoff@gmail.com>
2020-11-23 02:07:41 +0200

    test: added a test for the never combinator

--- src/combinators.test.ts
+++ src/combinators.test.ts
@@ -1,8 +1,15 @@
 import * as C from "."
 
 describe("combinators", () => {
+    describe("always<T, U>(v: U): Task<T, U>", () => {
+        it("always returns the parameter v", async () => {
+            const task = C.always("foo")
+
+            expect(task("bar")).toBe("foo")
+        })
+    })
     describe("never<T>(_: T): null", () => {
-        it("always returns null", async () => {
+        it("always returns null", () => {
             const task = C.never("foo")
 
             expect(task).toBeNull()
