41ef53db4db90f84516d2eaa5ce3dfd719d90fbd
BlueGhostGH <engisoftleaderoff@gmail.com>
2020-11-24 01:10:14 +0200

    test: added a test for the choose combinator

--- src/combinators.test.ts
+++ src/combinators.test.ts
@@ -2,14 +2,26 @@ import * as C from "."
 
 describe("combinators", () => {
     describe("always<T, U>(v: U): Task<T, U>", () => {
-        it("always returns the parameter v", async () => {
+        it("returns the parameter v", async () => {
             const task = C.always("foo")
 
-            expect(task("bar")).toBe("foo")
+            expect(await task("bar")).toBe("foo")
+        })
+    })
+    describe("choose<I, O>(Array<Task<I, O>>): Task<I, O>", () => {
+        it("returns never if there are no tasks", async () => {
+            const fail = C.choose<string, string>()
+
+            expect(await fail("foo")).toBeNull()
+        })
+        it("returns the matching task", async () => {
+            const task = C.choose(C.never, C.always("bar"))
+
+            expect(await task("foo")).toBe("bar")
         })
     })
     describe("never<T>(_: T): null", () => {
-        it("always returns null", () => {
+        it("returns null", () => {
             const task = C.never("foo")
 
             expect(task).toBeNull()
